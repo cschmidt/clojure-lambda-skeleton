2bb30c5f51154a021ef0ac6c941af5b75e012a4d
Carl Schmidt <carl.schmidt@gmail.com>
2017-01-05 09:50:27 -0800

    Parses and logs a request from API gateway

--- src/clojure_lambda_skeleton/core.clj
+++ src/clojure_lambda_skeleton/core.clj
@@ -1,15 +1,21 @@
 (ns clojure-lambda-skeleton.core
   (:import [com.amazonaws.services.lambda.runtime Context RequestHandler]
            [java.io InputStream OutputStream])
+  (:require [cheshire.core :refer [generate-string parse-stream]]
+            [clojure.java.io :refer [reader]])
   (:gen-class :implements [com.amazonaws.services.lambda.runtime.RequestHandler]
               :name ClojureRequestHandler
               :main false
               :methods [[handleRequest [java.io.InputStream
                                         java.io.OutputStream
-                                        com.amazonaws.services.lambda.runtime.Context] void]]
-              ))
+                                        com.amazonaws.services.lambda.runtime.Context] void]]))
+
+(defn query-param [request param-name]
+  (get-in request ["queryStringParameters" param-name]))
 
 (defn -handleRequest [this ^InputStream input ^OutputStream output ^Context context]
-  (let [logger (.getLogger context)]
-    (.log logger "Hey there!")))
+  (let [logger (.getLogger context)
+        request (parse-stream (reader input))]
+    (.log logger (str "request: " (generate-string request)))
+    (.log logger (str "a=" (query-param request "a")))))
 
